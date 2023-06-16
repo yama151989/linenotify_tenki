4f9b6dcc32694d98e8fef91810fbf53d3077cba8
香川 直希 <yama151989@gmail.com>
2023-06-16 20:53:56 +0900

    最新コミット

--- tenki.js
+++ tenki.js
@@ -1,42 +1,44 @@
-const axios = require('axios');
-const qs = require('querystring');
-const dotenv = require("dotenv").config();
-// import axios from 'axios';
-// import qs from 'querystring';
-// import * as dotenv from "dotenv";
-// dotenv.config();
+// main.mjsにマージ
 
-const ido = `${process.env.jitaku_ido}`;
-const keido = `${process.env.jitaku_keido}`;
-// console.log (ido);
-// console.log(keido);
+// const axios = require('axios');
+// const qs = require('querystring');
+// const dotenv = require("dotenv").config();
+// // import axios from 'axios';
+// // import qs from 'querystring';
+// // import * as dotenv from "dotenv";
+// // dotenv.config();
 
-class tenki {
-    constructor(){
+// const ido = `${process.env.jitaku_ido}`;
+// const keido = `${process.env.jitaku_keido}`;
+// // console.log (ido);
+// // console.log(keido);
 
-    }
-    // 天気情報取得
-    tenki_for() {
-        return axios({
-            method: 'GET',
-            url: `http://api.openweathermap.org/data/2.5/forecast?lon=${keido}&lat=${ido}&units=metric&lang=ja&appid=${process.env.API_KEY}`,
-        })
-    }
+// class tenki {
+//     constructor(){
 
-    // line通知
-    linePost(text,env) {
-        return axios({
-            method: 'post',
-            url: 'https://notify-api.line.me/api/notify',
-            headers: {
-                Authorization: `Bearer ${env}`,
-            },
-            data: qs.stringify({
-                message: text,
-            })
-        })
-    }
-}
+//     }
+//     // 天気情報取得
+//     tenki_for() {
+//         return axios({
+//             method: 'GET',
+//             url: `http://api.openweathermap.org/data/2.5/forecast?lon=${keido}&lat=${ido}&units=metric&lang=ja&appid=${process.env.API_KEY}`,
+//         })
+//     }
 
-module.exports = tenki;
-// export default tenki;
+//     // line通知
+//     linePost(text,env) {
+//         return axios({
+//             method: 'post',
+//             url: 'https://notify-api.line.me/api/notify',
+//             headers: {
+//                 Authorization: `Bearer ${env}`,
+//             },
+//             data: qs.stringify({
+//                 message: text,
+//             })
+//         })
+//     }
+// }
+
+// module.exports = tenki;
+// // export default tenki;
